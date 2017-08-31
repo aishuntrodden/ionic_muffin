db4665a0fec1447b69b32f544e7804cd116fa2e8
aishwary <aishwary.agnihotri0512@gmail.com>
2017-08-31 16:13:00 +0530

    login complete and data retrning

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,77 +2,93 @@ import { WelcomePage } from './../welcome/welcome';
 import { GetloginProvider } from './../../providers/getlogin/getlogin';
 import { Component, OnInit } from '@angular/core';
 import { NavController, } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   welcome = WelcomePage;
-  data: any;
-  response: any;
-  body: any;
+  results: any
   ngOnOnit() {
 
 
   }
-  constructor(public navCtrl: NavController, private GetloginService: GetloginProvider) {
+  constructor(public navCtrl: NavController, private GetloginService: GetloginProvider, private alertCtrl: AlertController) {
 
   }
-  ionViewWillEnter() {
+  send() {
+    window.location.href = 'http://ec2-54-198-217-196.compute-1.amazonaws.com:8000/authe';
 
+  }
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Login',
+
+      buttons: ['Dismiss']
+    });
+    alert.present();
+    window.location.href = 'http://ec2-54-198-217-196.compute-1.amazonaws.com:8000/authe';
+    this.ionViewWillLeave();
+  }
+  ionViewWillEnter() {
+    this.loadComments();
 
   }
   ionViewWillLeave() {
 
-    this.loadComments();
+
     // this.abc();
 
 
   }
-
+  public data: any;
+  response: any;
+  body: any;
   loadComments() {
 
-    // Get all comments
     this.GetloginService.getComments()
       .subscribe(
-      data => { this.data = data; }, //Bind to view
-
+      data => {
+        this.data = data;
+        this.set(this.data);
+        // console.log(this.data);
+      }, //Bind to view
       err => {
         // Log errors if any
         console.log(err);
       });
+
+
+  }
+  abc() {
+    console.log(this.body);
+  }
+  a() {
+    this.navCtrl.push(WelcomePage);
+  }
+  set(data) {
     this.body = {
-      email: this.data.email,
-      id_token: this.data.auth.params.id_token,
-      name: this.data.name.givenName,
-      provider: this.data.provider,
-      uid: this.data.id,
-      token_type: this.data.auth.params.token_type,
-      oauth_expires_at: this.data.auth.params.expires_at,
-      expires_in: this.data.auth.params.expires_in,
-      display_name: this.data.name.givenName,
-      oauth_token: this.data.auth.params.access_token,
-      photo_url: this.data.photos[0].value,
-      refresh_token: this.data.auth.refreshToken,
+      email: data.email,
+      id_token: data.auth.params.id_token,
+      name: data.name.givenName,
+      provider: data.provider,
+      uid: data.id,
+      token_type: data.auth.params.token_type,
+      oauth_expires_at: data.auth.params.expires_at,
+      expires_in: data.auth.params.expires_in,
+      display_name: data.name.givenName,
+      oauth_token: data.auth.params.access_token,
+      photo_url: data.photos[0].value,
+      refresh_token: data.auth.refreshToken,
       email_to: "aishwary.athlete@gmail.com",
       body: "Hitt this mail",
       subject: "This is test mail",
       to: "mayankchandelchat@gmail.com"
 
     };
-
-  }
-  abc() {
     console.log(this.body);
-  }
-  send() {
-    this.loadComments();
-    window.location.href = 'http://ec2-54-198-217-196.compute-1.amazonaws.com:8000/authe';
-
-  }
-  a() {
-    this.navCtrl.push(WelcomePage);
   }
 
 }
